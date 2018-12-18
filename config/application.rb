27a7945bb55571afb4bd592fbfad6381559b0c32
eclero <eclero357@gmail.com>
2018-12-19 03:12:49 +0900

    Fix codes according to Rubocop

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,17 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-# require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+# require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -33,10 +33,10 @@ module Myapp
     config.api_only = true
     config.generators do |g|
       g.test_framework :rspec,
-      controller_specs: false,
-      helper_specs:     false,
-      routing_specs:    false,
-      view_specs:       false
-      end
+        controller_specs: false,
+        helper_specs:     false,
+        routing_specs:    false,
+        view_specs:       false
+    end
   end
 end
